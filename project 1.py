c0ce11c963350838d6b1e11748975dbc2bf0fb25
sud0013 <sudarshan.b5536@gmail.com>
2024-07-01 20:14:50 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,11 @@
+# Python Program To Convert Fahrenheit to Celsius
+
+# temp= float(input("Enter temperature in Fahrenheit: "))
+# celsius=(temp - 32) * 5/9
+# round(celsius,2)
+# print("Temperature in Celsius:")
+
+
+fahrenheit = float(input("Please enter temperature in fahrenheit:"))
+celsius = (fahrenheit - 32) * 5 / 9
+print("fahrenheit in celsius: ", round(celsius,3))
